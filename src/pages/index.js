1ac2e75d5cd87e0e386e18751f6fafdda5fa8cfd
Vitaliy Stanyshevskyy <vstanyshevskyy@stelladot.com>
2019-05-02 13:09:15 +0300

    Print title and copyright

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,17 +4,29 @@ import "./index.less"
 import User from "../components/user/"
 
 export default ({ data }) => {
-  console.log(data);
   const users = data.allRandomUser.edges;
+  const { copyright, title } = data.markdownRemark.frontmatter;
   return (
-    <div className="users-container">
-      {users.map((user, i) => <User user={user.node} />)}
+    <div className="app">
+      <h1>{title}</h1>
+
+      <div className="users-container">
+        {users.map((user, i) => <User user={user.node} />)}
+      </div>
+      
+      <p>{copyright}</p>
     </div>
   );
 }
 
 export const query = graphql`
   query HomePageData {
+    markdownRemark {
+      frontmatter {
+        title
+        copyright
+      }
+    }
     allRandomUser {
       edges {
         node {
